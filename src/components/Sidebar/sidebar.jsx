698af945c753686671f7fce47228193a44101d8a
Naufal Taufiqurrahman <naufaltq16@gmail.com>
2022-04-21 16:02:56 +0700

    create layout

---
+++ src/components/Sidebar/sidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sidebar.css";
+
+function Sidebar() {
+  return <div className="sidebar-content">Sidebar</div>;
+}
+
+export default Sidebar;
--- src/components/Spotify/spotify.jsx
+++ src/components/Spotify/spotify.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Body from "../Body/body";
+import Footer from "../Footer/footer";
+import Navbar from "../Navbar/navbar";
+import Sidebar from "../Sidebar/sidebar";
+import "./spotify.css";
 
 function Spotify() {
-  return <div>spotify</div>;
+  return (
+    <div className="container">
+      <div className="spotify-body">
+        <Sidebar />
+        <div className="body">
+          <Navbar />
+          <div className="body-contents">
+            <Body />
+          </div>
+        </div>
+      </div>
+      <div className="spotify-footer">
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default Spotify;
